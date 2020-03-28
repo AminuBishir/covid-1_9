83ebbc0d9a5a0818d2aa0a5712df3542c9f3eeeb
Aminu Bishir <aminubishir@gmail.com>
2020-03-28 09:13:03 +0100

    Update major-city-plots.js
    11:55pm 27/03/2020 new case announced
    8 in Lagos, 2 in Enugu and 1 in Edo

--- scripts/major-city-plots.js
+++ scripts/major-city-plots.js
@@ -25,7 +25,7 @@ fetch("https://raw.githubusercontent.com/AminuBishir/covid-1_9/master/data/ng_st
             
         if(total_confirmed >0){
 			
-			document.getElementById('breaking').innerHTML += '<font style="color:red;font-weight:bold;margin-left:5px;font-size:20px">Covid19 Alert: </font><font style="margin-left:5px;font-size:18px">5 new cases have been confirmed by NCDC (3 in FCT and 2 in Oyo), as at 8:00pm 27/03/2020, bringing the total confirmed cases to <font style="color:red;font-weight:bold;margin-left:5px;font-size:20px">'+total_confirmed+'</font></font>'
+			document.getElementById('breaking').innerHTML += '<font style="color:red;font-weight:bold;margin-left:5px;font-size:20px">Covid19 Alert: </font><font style="margin-left:5px;font-size:18px">11 new cases have been confirmed by NCDC (8 in Lagos, 2 in Enugu and 1 in Edo), as at 11:55pm 27/03/2020, bringing the total confirmed cases to <font style="color:red;font-weight:bold;margin-left:5px;font-size:20px">'+total_confirmed+'</font></font>'
 		}
 	$("#map-area").mapael({
 			map: {
